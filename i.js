542ee8f7034909ff93989049146fcc9865927fff
Devin Carr <devin.carr0@gmail.com>
2017-10-17 19:37:43 -0700

    added iteration input

--- i.js
+++ i.js
@@ -1,29 +1,42 @@
 ((exports) => {
     let tau = 2 * Math.PI,
         svg = d3.select("div#i").append("svg")
-                     .attr("height", 400)
-                     .attr("width", 400)
-                     .attr("viewBox", "-500 -500 1000 1000")
+                     .attr("height", 800)
+                     .attr("width", 800)
+                     .attr("viewBox", "-2000 -2000 4000 4000"),
+        input = document.getElementById("input"),
+        inputButton = document.getElementById("inputButton")
 
-    let zip = (rowLeft,rowRight,f) => rowLeft.map((i,c) => f(i,rowRight[c]))
-    let zipPoints = (rowLeft,rowRight,f) => zip(rowLeft,rowRight,(l,r) => zip(l,r,f))
+    let getIterations = () => {
+        let query = location.search.slice(1)
+        let num = Number.parseInt(query)
+        if (Number.isNaN(num)) return 7
+        return num
+    }
+    let interzip = (row,rowInner) => {
+        let combined = Array((row.length * 2) - 2)
+        let a = 0, b = 0
+        for (let i = 0; i < combined.length; i++) {
+            combined[i] = i % 2 ? rowInner[b++] : row[a++]
+        }
+        return combined
+    }
     let draw = (pointArray) => {
         let poly = pointArray.map(p => { return p.join(',') }).join(' ')
-        console.log(poly)
         svg.append("polygon")
             .attr("points", poly)
     }
     let ngon = (radius,sides) => {
         let angles = [],
             angle = (tau - 0.000001) / sides
-        for (var i = 0; i < sides; i++) angles.push(i * angle)
+        for (let i = 0; i < sides; i++) angles.push(i * angle)
         return angles.map(a => {
            return [radius * Math.cos(a), radius * Math.sin(a)]
         })
     }
     let irregular = (pts) => {
         return pts.map(p => {
-            var r = Math.random()
+            let r = Math.random() + 1
             return [r * p[0], r * p[1]]
         })
     }
@@ -36,16 +49,23 @@
         return [mx + d * vx, my + d * vy]
     }
     let loopAround = (pts) => {
-        return pts.concat([pts[0]]);
+        return pts.concat([pts[0]])
     }
     let subdivideWith = (f, pts) => {
-        if (pts.length === 1) return [pts[0]]
-        return [pts[0],f(pts[0],pts[1])]
-                .concat(subdivideWith(f,pts.slice(1)))
+        let newPoints = []
+        pts = loopAround(pts)
+        for (let i = 0; i < pts.length - 1; i++) {
+            newPoints.push(f(pts[i],pts[i+1]))
+        }
+        return interzip(pts,newPoints)
     }
-    var p = loopAround(irregular(ngon(700,3)))
+    var p = irregular(ngon(700,3))
     var island = subdivideWith(offsetMidpoint,p)
-    for (var i = 0; i < 5; i++)
+    for (var i = 0; i < getIterations(); i++)
         island = subdivideWith(offsetMidpoint,island)
     draw(island)
+
+    inputButton.onclick = () => {
+        location.href = 'https:\\\\' + location.host + '/?' + input.value
+    }
 })(window);
